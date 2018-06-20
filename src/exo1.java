91b85701ec389e130aeaa87fc3e9df60ca4a2661
mamat <matmat489@hotmail.fr>
2018-06-20 10:51:42 +0200

    nouveau

--- src/exo1.java
+++ src/exo1.java
@@ -1,15 +1,15 @@
+import java.lang.*;
 public class exo1 {
 
 
     public static void main(String[] args){
         exo1 test = new exo1();
         test.transformation();
-
     }
 
     public void transformation() {
-        int tabModule[] = {1, 1,1,1};
-        int tabArgument[] = {2,3,1,0};
+        int tabModule[] = {1,1,1,1};
+        int tabArgument[] = {1,2,3,4};
         Cplx resultat[]=new CplxPol[4];
 
         Cplx[] complexe = new CplxPol[4];
@@ -19,22 +19,47 @@ public class exo1 {
             System.out.println(complexe[i].toString());
         }
 
-        for (int i = 0; i < complexe.length; i++) {
+        for (int i=0; i < complexe.length; i++) {
             Cplx temp1 = new CplxPol(0,0);
             temp1=omega(i,complexe.length,1);
+            System.out.println("temp1: "+temp1);
+
+            //sors en -infinity argu
             temp1.multiplication(somme(complexe,i)[1]);
+            System.out.println("temp2: "+temp1);
+
             temp1.ajoute(somme(complexe,i)[0]);
+            System.out.println("temp3: "+temp1);
+
+            System.out.println("\n\n");
+
+
             resultat[i]= temp1;
             //resultat[i] = somme(complexe,i)[0]+omega(i,complexe.length,1)*somme(complexe,i)[1];
 
         }
         for (int i = 0; i < complexe.length; i++) {
-            System.out.println(resultat[i].toString());
+            //System.out.println(resultat[i].toString());
         }
     }
 
     public Cplx omega(int puissance, int facteur,int k){
-        Cplx complexe = new CplxPol(1, -2*Math.PI/(puissance*k/(facteur)));
+        Cplx complexe;
+        if(puissance==0 || k==0){
+            complexe = new CplxPol(1, 0);
+        }
+        else {
+            complexe = new CplxPol(1, -2 * Math.PI / ((float) (puissance * k) / (facteur)));
+        }
+        /*float div=((puissance*k)/(facteur));
+        System.out.println("div= "+div);
+        System.out.println((float)(puissance*k)/facteur);
+
+        System.out.println("ok"+(float)((-2*Math.PI)/div));
+        System.out.println("fin"+((puissance*k)));
+        System.out.println("facteur"+facteur);*/
+
+
         return complexe;
     }
 
@@ -42,17 +67,18 @@ public class exo1 {
     public Cplx[] somme(Cplx[] liste,int k) { //r cest i et k cest k ici
         Cplx sommepaire = new CplxPol(0,0);
         Cplx sommeimpaire = new CplxPol(0,0);
-        Cplx tempimpaire = new CplxPol(0,0);
-        Cplx temppaire = new CplxPol(0,0);
+        Cplx tempimpaire;
+        Cplx temppaire;
         for(int i = 0; i <= (liste.length/2)-1; i++) {
 
             //IMPAIRE
-            tempimpaire=omega(i,liste.length,k);
+            tempimpaire=omega(i,liste.length/2,k);
             tempimpaire.multiplication(liste[2*i]);
+            System.out.println("tempimpaire: "+tempimpaire);
             sommeimpaire.ajoute(tempimpaire);
 
             //Paire
-            temppaire=omega(i,liste.length,k);
+            temppaire=omega(i,liste.length/2,k);
             temppaire.multiplication(liste[2*i+1]);
             sommepaire.ajoute(tempimpaire);
         }
